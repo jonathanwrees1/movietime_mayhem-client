a329156b0a395fa80dc3d07559081763fc3e5d86
jonathanwrees1 <97190782+jonathanwrees1@users.noreply.github.com>
2022-03-10 18:59:32 -0800

    Added Details to movie info, and new image urls

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -5,16 +5,34 @@ export class MovieView extends React.Component {
         const { movie, onBackClick } = this.props;
         return( <div className="movie-view">
                 <div className="movie-poster">
-                    <img src={movie.ImagePath} alt= "image"/>
-                </div>
+                <img src={movie.ImagePath} height={400} width={300} alt= "image" crossOrigin="anonymous" />
+                </div><br></br>
+
                 <div className="movie-title">
                     <span className="label">Title: </span>
                     <span className="value">{movie.Title}</span>
-                    </div>
+                    </div><br></br>
+
                     <div className="movie-description">
                         <span className="label">Description: </span>
-                        <span className="value">{movie.Description}</span>
-                    </div>
+                        <span className="value">{movie.Description} </span>
+                    </div><br></br>
+
+                    <div className= "movie-genre">
+                        <span className="label">Genre: </span>
+                        <span className="value">{movie.Genre}</span>
+                    </div><br></br>
+
+                    <div className= "movie-director">
+                        <span className="label">Director: </span>
+                        <span className="value">{movie.Director}</span>
+                    </div><br></br>
+
+                    <div className= "movie-released">
+                        <span className="label">Realease Date: </span>
+                        <span className="value">{movie.Released}</span>
+                    </div><br></br>
+
                     <button onClick={() => { onBackClick(null); }}>Back</button>
 
                     </div>
